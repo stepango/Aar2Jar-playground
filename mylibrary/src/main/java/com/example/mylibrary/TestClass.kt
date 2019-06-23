3c5250ec30e762de28c05169abbb7dd4463a1a6e
stepan.goncharov <step.89.g@gmail.com>
2019-06-23 14:45:01 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/TestClass.kt
@@ -0,0 +1,10 @@
+package com.example.mylibrary
+
+import android.content.Context
+import androidx.appcompat.app.ActionBar
+import androidx.lifecycle.LiveData
+
+class TestClass {
+    fun test(context: Context, bar: ActionBar, liveData: LiveData<*>) {
+    }
+}
